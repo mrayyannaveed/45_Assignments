db7aff7191231a6fe9c9b118f51df25d1d2619a1
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-03-22 08:25:54 +0500

    Add title case in Project_03

--- Project_03_NameCases/app.js
+++ Project_03_NameCases/app.js
@@ -1,4 +1,8 @@
-var personName = "Rayyan";
+"use strict";
+let personName = "Rayyan Naveed";
 console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
-console.log(personName);
+console.log(personName
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" "));
--- Project_03_NameCases/app.ts
+++ Project_03_NameCases/app.ts
@@ -1,4 +1,9 @@
-let personName = "Rayyan"
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName);
+let personName = "Rayyan Naveed";
+console.log("Lower Case" +personName.toLowerCase());
+console.log("Upperr Case" +personName.toUpperCase());
+console.log("Title Case" +
+  personName
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ")
+);
